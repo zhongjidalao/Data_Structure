5ae9a86ae680e36b03fcb5c10ce7f41d494631eb
zhongjidalao <mdd12138@163.com>
2019-11-06 21:49:22 +0800

    斐波那契的递归函数

--- Stack/stack_linked_list.c
+++ Stack/stack_linked_list.c
@@ -28,3 +28,35 @@ Status Push(LinkStack *S, SElemType e){
 	S->count++;
 	return OK;
 }
+
+
+//斐波那契数列
+// int main(){
+// 	int i;
+// 	int a[40];
+// 	a[0] = 0;
+// 	a[1] = 1;
+// 	printf("%d\n", a[0]);
+// 	printf("%d\n", a[1]);
+// 	for(i = 2; i<40; i++){
+// 		a[i] = a[i-1] + a[i-2];
+// 		printf("%d\n", a[i]);
+// 	}
+// 	return 0;
+// }
+
+//斐波那契数列的递归函数
+int Fbi(int i){
+	if(i < 2){
+		return i == 0 ? 0 : 1;
+	}
+	return Fbi(i-1) + Fbi(i-2);//这里Fbi就是函数自己，它在调用自己
+}
+
+int main(){
+	int i;
+	for(int i=0; i<40; i++){
+		printf("%d\n", Fbi(i));
+	}
+	return 0;
+}
