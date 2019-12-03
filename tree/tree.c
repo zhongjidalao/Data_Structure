abe082f27631ecf72479aeabe4a6c084c0605ebe
zhongjidalao <mdd12138@163.com>
2019-12-04 01:13:07 +0800

    树的结构

--- tree/tree.c
+++ tree/tree.c
@@ -11,8 +11,28 @@ typedef struct PTNode{
     int parent;
 }PTNode;
 
+//孩子结点
+typedef struct CTNode{
+    int chile;
+    struct CTNode *next;
+} *ChildPtr;
+
+//表头结构
+typedef struct{
+    TElemType data;
+    ChildPtr firstchild;
+}CTBox;
+
 //树结构
 typedef struct{
-    PTNode nodes[MAX_TREE_SIZE];
+    //结点数组
+    CTBox nodes[MAX_TREE_SIZE];
+    //根的位置和结点数
     int r,n;
-}PTree;
+}CTree;
+
+//树的孩子兄弟表示法结构定义
+typedef struct CSNode{
+    TElemType data;
+    struct CSNode *firstchild,*rightsib;
+}CSNode,*CSTree;
