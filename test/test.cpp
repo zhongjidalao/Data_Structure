52b66278c8859856a99aa0f163be7f6ddb1fd94b
madandan <mdd12138@163.com>
2020-08-05 10:32:43 +0800

    c++测试

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    float y;
+    cout<<"Please input an int number:"<<endl;
+    cin>>x;
+    cout<<"The int number is x= "<<x<<endl;
+    cout<<"Please input a float number:"<<endl;
+    cin>>y;
+    cout<<"The float number is y= "<<y<<endl;
+    return 0;
+}
